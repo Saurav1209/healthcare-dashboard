3d33371fb091435477c6b8c60175420820092338
saurav <sauravlokhande24@gmail.com>
2024-12-01 13:52:30 +0530

    nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../Styles/navbar.css'
+function navbar() {
+  return (
+    <div className='nav'>
+    <div className='testLogo'></div>
+    <div className='nav-ele-box'>
+        <div className='nav-ele-home-img'></div>
+        <div className='nav-ele-over'>Overview</div>
+        <div className='nav-ele-user-img'></div>
+        <div className='nav-ele-cal-img'> </div>
+        <div className='nav-ele-msg-img'> </div>
+        <div className='nav-ele-tran-img'> </div>
+        
+    </div>
+    <div className='nav-right'>
+        <div className='login-img'></div>
+        <div className='login-name'>Dr. Jose Simmons</div>
+        <div className='login-name-prac'> Genral Practitioner</div>
+    </div>
+    </div>
+  )
+}
+
+export default navbar
